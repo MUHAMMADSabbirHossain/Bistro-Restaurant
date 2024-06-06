5f4e8d64b2c06f2f189b84664f979a66e4cd7bf7
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-06 22:16:13 +0600

    featured section added.

--- Bistro-Restaurant-Client/src/components/SectionTitle/SectionTitle.jsx
+++ Bistro-Restaurant-Client/src/components/SectionTitle/SectionTitle.jsx
@@ -1,6 +1,6 @@
 const SectionTitle = ({ heading, subHeading }) => {
     return (
-        <div className="mx-auto text-center md:w-4/12 my-8">
+        <div className="mx-auto text-center md:w-4/12 pt-8 my-10">
             <p className="text-yellow-600 mb-2">--- {subHeading} ---</p>
 
             <h3 className="border-y-4 uppercase text-3xl py-4">{heading}</h3>
