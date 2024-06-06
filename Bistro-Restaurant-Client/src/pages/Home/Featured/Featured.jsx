bd0b4f62b7af77752c375a069d31a9ad7c75e9a0
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-06 22:22:50 +0600

    paralax in featred section...

--- Bistro-Restaurant-Client/src/pages/Home/Featured/Featured.jsx
+++ Bistro-Restaurant-Client/src/pages/Home/Featured/Featured.jsx
@@ -4,17 +4,17 @@ import "./Featured.css";
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white">
+        <div className="featured-item bg-fixed text-white">
             <SectionTitle
                 heading={`Featured Item`}
                 subHeading={`Check it out`}></SectionTitle>
-            <div className="md:flex justify-center items-center pt-12 pb-20 px-36">
+            <div className="md:flex justify-center items-center bg-opacity-50 pt-12 pb-20 px-36">
                 <div><img src={featured} alt="" /></div>
                 <div className="md:ml-10">
                     <p>Aug 20, 2029</p>
                     <p className="">Where can i get some?</p>
                     <p>Food is usually of plant, animal, or fungal origin and contains essential nutrients such as carbohydrates, fats, proteins, vitamins, or minerals. The substance is ingested by an organism and assimilated by the organism cells to provide energy, maintain life, or stimulate growth.</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4">Order Now</button>
                 </div>
             </div>
         </div>
