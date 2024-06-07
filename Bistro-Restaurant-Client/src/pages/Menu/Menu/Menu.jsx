7e90702ed0772102c34e09d90c526d0e7213adf0
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-07 12:09:08 +0600

    react-parallax added, cover.jsx added with parallax...

--- Bistro-Restaurant-Client/src/pages/Menu/Menu/Menu.jsx
+++ Bistro-Restaurant-Client/src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
     return (
@@ -6,7 +9,20 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Menu</title>
             </Helmet>
-            <h2>menu</h2>
+            <Cover
+                img={menuImg}
+                title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover
+                img={menuImg}
+                title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover
+                img={menuImg}
+                title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
