6f4871671c10261c590b557f4fe7da02aad2331f
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-11 22:41:09 +0600

    adminroute syntex bug fixed.

--- Bistro-Restaurant-Client/src/routes/AdminRoute.jsx
+++ Bistro-Restaurant-Client/src/routes/AdminRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate, useLocation } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const AdminRoute = (children) => {
+const AdminRoute = ({ children }) => {
 
-    const [user, loading] = useAuth();
+    const { user, loading } = useAuth();
     const [isAdmin, isAdminLoading] = useAdmin();
     const location = useLocation();
 
@@ -16,7 +16,7 @@ const AdminRoute = (children) => {
         return children;
     }
 
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    return <Navigate to="/" state={{ from: location }} replace></Navigate>
 };
 
 export default AdminRoute;
