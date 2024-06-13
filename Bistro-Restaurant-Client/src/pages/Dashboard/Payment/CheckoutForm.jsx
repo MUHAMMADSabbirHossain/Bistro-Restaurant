559371257e932f144d47dc57b9494bd4b84d81d1
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-13 15:04:12 +0600

    swal added after payment success...

--- Bistro-Restaurant-Client/src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ Bistro-Restaurant-Client/src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
 import useAuth from '../../../hooks/useAuth';
+import Swal from "sweetalert2";
 
 const CheckoutForm = () => {
 
@@ -13,15 +14,17 @@ const CheckoutForm = () => {
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
     const { user } = useAuth();
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
     useEffect(() => {
-        axiosSecure.post("/create-payment-intent", { price: totalPrice })
-            .then(res => {
-                console.log(res.data.clientSecret);
-                setClientSecret(res.data.clientSecret);
-            })
+        if (totalPrice > 0) {
+            axiosSecure.post("/create-payment-intent", { price: totalPrice })
+                .then(res => {
+                    console.log(res.data.clientSecret);
+                    setClientSecret(res.data.clientSecret);
+                })
+        }
     }, [axiosSecure, totalPrice])
 
     const handleSubmit = async (event) => {
@@ -89,6 +92,18 @@ const CheckoutForm = () => {
 
                 const res = await axiosSecure.post('/payments', payment);
                 console.log("Payment save: ", res.data);
+
+                refetch();
+
+                if (res.data?.paymentResult?.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Thank you for the Taka Poisha.",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
             }
         }
     };
