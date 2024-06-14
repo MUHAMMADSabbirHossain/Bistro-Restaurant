1518cbf508ed5638bf385096dfc69e116369c512
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2024-06-14 23:36:22 +0600

    client fetch url change and deploy to firebase successfully...

--- Bistro-Restaurant-Client/src/pages/Home/Testimonials/Testimonials.jsx
+++ Bistro-Restaurant-Client/src/pages/Home/Testimonials/Testimonials.jsx
@@ -19,7 +19,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/reviews")
+        fetch("https://bistro-testaurant-server.vercel.app/reviews")
             .then(res => res.json())
             .then(data => {
                 console.log(data);
